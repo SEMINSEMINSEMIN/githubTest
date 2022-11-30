74ea1674a4ad670b74b4cd5bff6b4887e920841f
강세민 <seminkang@gangsemin-ui-iMac.local>
2022-11-30 17:40:01 +0900

    [Edit] 계산기-나누기에서 나누는 수가 0일 경우 처리
    나누는 수가 0일 경우 Infinity 리턴 대신, alert창을 띄우도록 바꿨다.
    
    Resolves #2

--- calculator.js
+++ calculator.js
@@ -17,6 +17,10 @@ class Calculator {
   }
 
   div() {
+    if (this.value2 === 0) {
+      window.alert("나누는 수는 0이 되어서는 안 됩니다.");
+      return;
+    }
     return this.value1 / this.value2;
   }
 }
